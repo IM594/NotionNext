b2d9018e7cce028b6590cdc1383d4b2105cb0f58
Zhaohao Lu <69454774+IM594@users.noreply.github.com>
2024-04-30 01:35:41 +0800

    misc: modify heo UI

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -67,8 +67,8 @@ const LayoutBase = props => {
       {/* 通知横幅 */}
       {router.route === '/'
         ? <>
-            <NoticeBar />
-            <Hero {...props} />
+            {/* <NoticeBar />
+            <Hero {...props} /> */}
         </>
         : null}
       {fullWidth ? null : <PostHeader {...props} />}
